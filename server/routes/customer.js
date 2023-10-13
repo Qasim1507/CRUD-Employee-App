0acf42f2c49b45c282eeddf713cb0905b13827f5
nalawalaq <nalawalaq@gmail.com>
2023-10-13 14:57:46 +0400

    bug fix 4

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -27,44 +27,65 @@ const fileUpload = multer();
 router.post("/add", fileUpload.single("file"), async (req, res) => {
   try {
     if (!req.file) {
-      // Handle the case when no file is uploaded
-      console.log("No file uploaded.");
-    }
+      let devices = [];
+      if (req.body.devices && Array.isArray(req.body.devices)) {
+        devices = req.body.devices;
+      }
 
-    const uploadImage = new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result.url);
-        } else {
-          reject(error);
-        }
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        designation: req.body.designation,
+        email: req.body.email,
+        credentials: req.body.credentials,
+        dept: req.body.dept,
+        handover: imageUrl,
+        devices: devices,
+        antivirus: req.body.antivirus,
       });
 
-      streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
+      await User.create(newUser);
 
-    const imageUrl = await uploadImage;
-    let devices = [];
-    if (req.body.devices && Array.isArray(req.body.devices)) {
-      devices = req.body.devices;
+      res.redirect("/dashboard");
     }
 
-    const newUser = new User({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      tel: req.body.tel,
-      designation: req.body.designation,
-      email: req.body.email,
-      credentials: req.body.credentials,
-      dept: req.body.dept,
-      handover: imageUrl,
-      devices: devices,
-      antivirus: req.body.antivirus,
-    });
+    if (req.file) {
+      const uploadImage = new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result.url);
+          } else {
+            reject(error);
+          }
+        });
 
-    await User.create(newUser);
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
 
-    res.redirect("/dashboard");
+      const imageUrl = await uploadImage;
+      let devices = [];
+      if (req.body.devices && Array.isArray(req.body.devices)) {
+        devices = req.body.devices;
+      }
+
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        designation: req.body.designation,
+        email: req.body.email,
+        credentials: req.body.credentials,
+        dept: req.body.dept,
+        handover: imageUrl,
+        devices: devices,
+        antivirus: req.body.antivirus,
+      });
+
+      await User.create(newUser);
+
+      res.redirect("/dashboard");
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Image upload failed");
